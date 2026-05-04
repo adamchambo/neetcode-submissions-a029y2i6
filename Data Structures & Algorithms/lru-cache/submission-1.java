bb12938c6740949fd3c89ac2f3c98207ef6a687b
Adam Chamberlain <110888662+adamchambo@users.noreply.github.com>
2026-05-04 18:42:47 +1000

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.java
@@ -0,0 +1,60 @@
+class LRUCache {
+    private class Node {
+        int key, value;
+        Node prev, next;
+        Node(int k, int v) { key = k; value = v; }
+    }
+
+    private final int capacity;
+    private Map<Integer, Node> map;
+    private Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.map = new HashMap<>(capacity);
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+        Node node = map.get(key);
+        remove(node);
+        insert(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node existing = map.get(key);
+            remove(existing);
+        }
+
+        Node node = new Node(key, value);
+        map.put(key, node);
+        insert(node);
+
+        if (map.size() > capacity) {
+            Node lru = head.next; 
+            remove(lru);
+            map.remove(lru.key);
+
+        }
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insert(Node node) {
+        Node prev = tail.prev;
+        prev.next = node;
+        node.prev = prev;
+        node.next = tail;
+        tail.prev = node;
+    }
+
+}
