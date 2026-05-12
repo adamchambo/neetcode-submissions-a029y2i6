3372964817ea39089d5b044075f6bcffefad3a1c
Adam Chamberlain <110888662+adamchambo@users.noreply.github.com>
2026-05-12 18:48:44 +1000

    Add: flood-fill - submission-0

---
+++ Data Structures & Algorithms/flood-fill/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int original = image[sr][sc];
+        dfs(image, sr, sc, original, color);
+        return image;
+    }
+
+    private void dfs(int[][] image, int sr, int sc, int original, int color) {
+        if (sr < 0 || sr >= image.length ||
+            sc < 0 || sc >= image[0].length) {
+            return;
+        }
+        if (original == color) return;
+        if (image[sr][sc] != original) return;
+
+        image[sr][sc] = color;
+
+        dfs(image, sr+1, sc, original, color);
+        dfs(image, sr-1, sc, original, color);
+        dfs(image, sr, sc+1, original, color);
+        dfs(image, sr, sc-1, original, color);
+    }
+}
